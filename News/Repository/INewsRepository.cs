07d3c0654b54f4401b0cb9bc33e22a96ae9d0bf7
Fredrik Leijon <fredrik.leijon@gmail.com>
2013-08-05 10:50:18 +0200

    stuffs

---
+++ News/Repository/INewsRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using News.Models;
+
+namespace News.Repository
+{
+    public interface INewsRepository
+    {
+        void Add(NewsItem item);
+        IList<NewsItem> GetItems();
+    }
+}
--- News/Repository/NewsRepository.cs
+++ News/Repository/NewsRepository.cs
@@ -1,16 +1,20 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using News.Models;
 
 namespace News.Repository
 {
-    public interface INewsRepository
+    public class NewsRepository : INewsRepository, IDisposable
     {
-        void Add(NewsItem item);
-        IList<NewsItem> GetItems();
-    }
+        private SqlConnection conn;
 
-    public class NewsRepository : INewsRepository
-    {
+        public NewsRepository()
+        {
+            this.conn = new SqlConnection(ConfigurationManager.ConnectionStrings["connstr"].ConnectionString);
+        }
+
         public void Add(NewsItem item)
         {
             throw new System.NotImplementedException();
@@ -19,6 +23,15 @@ namespace News.Repository
         public IList<NewsItem> GetItems()
         {
             throw new System.NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            if (this.conn != null)
+            {
+                this.conn.Dispose();
+                this.conn = null;
+            }
         }
     }
 }
